113048676f196fb17d059ebe27e651f3b16232b4
balobasi <balobasi5@gmail.com>
2013-01-30 11:57:36 +0900

    Revert "Revert "aa""
    This reverts commit 2e67c25cccb71bc740e3d1b9a7e0b017033d89f0.

---
+++ index.php
@@ -0,0 +1,60 @@
+<?
+include_once("./_common.php");
+include_once("$g4[path]/lib/latest.lib.php");
+
+$g4['title'] = ""; 
+include_once("./_head.php");
+?>
+<div id="s1" >
+	<img src="<?=$g4['path']?>/img/banner01_01.png" width=675 height=200 alt="" />
+	<img src="<?=$g4['path']?>/img/banner01_02.png" width=675 height=200 alt="" />
+	<img src="<?=$g4['path']?>/img/banner01_03.png" width=675 height=200 alt="" />
+	<img src="<?=$g4['path']?>/img/banner01_04.png" width=675 height=200 alt="" />
+	<img src="<?=$g4['path']?>/img/bannar02_01.png" width=675 height=200 alt="" />
+	<img src="<?=$g4['path']?>/img/bannar02_03.png" width=675 height=200 alt="" />
+</div>
+<div class="h05"></div>
+<div id="pack_event">
+	<div class="pack_title">&nbsp; 여수골프장 패키지 투어</div>
+	<div class="pack_body">
+		<div class="pack">
+				<img src="<?=$g4['path']?>/img/pack_di.png" onMouseover="this.src='<?=$g4[path]?>/img/pack_dio.png'" onMouseout="this.src='<?=$g4[path]?>/img/pack_di.png'"
+	 				onclick="location.href='<?=$g4['path']?>/bbs/board.php?bo_table=golfbooking&type_menu=pakge&pakgetype=aa'"/>
+		</div>
+		<div class="pack" style="border-right:1px solid silver; border-left:1px solid silver;">
+				<img src="<?=$g4['path']?>/img/pack_ci.png" onMouseover="this.src='<?=$g4[path]?>/img/pack_cio.png'" onMouseout="this.src='<?=$g4[path]?>/img/pack_ci.png'" 
+					 onclick="location.href='<?=$g4['path']?>/bbs/board.php?bo_table=golfbooking&type_menu=pakge&pakgetype=bb'"/>
+		</div>
+		<div class="pack">
+				<img src="<?=$g4['path']?>/img/pack_gd.png" onMouseover="this.src='<?=$g4[path]?>/img/pack_gdo.png'" onMouseout="this.src='<?=$g4[path]?>/img/pack_gd.png'"
+			 		onclick="location.href='<?=$g4['path']?>/bbs/board.php?bo_table=golfbooking&type_menu=pakge&pakgetype=cc'"/>
+		</div>
+	</div>
+</div>
+<div class="h05"></div>
+<div id="s2" >
+	<img src="<?=$g4['path']?>/img/golf_banner01.png" width=675 height=300 onclick="location.href='<?=$g4['path']?>/bbs/board.php?bo_table=test_booking&type_menu=golf'" />
+	<img src="<?=$g4['path']?>/img/golf_banner02.png" width=675 height=300 onclick="location.href='<?=$g4['path']?>/bbs/board.php?bo_table=test_booking&type_menu=golf'" />
+	<img src="<?=$g4['path']?>/img/golf_banner03.png" width=675 height=300 onclick="location.href='<?=$g4['path']?>/bbs/board.php?bo_table=test_booking&type_menu=golf'" />
+	<img src="<?=$g4['path']?>/img/golf_banner04.png" width=675 height=300 onclick="location.href='<?=$g4['path']?>/bbs/board.php?bo_table=test_booking&type_menu=golf'" />
+	<img src="<?=$g4['path']?>/img/golf_banner05.png" width=675 height=300 onclick="location.href='<?=$g4['path']?>/bbs/board.php?bo_table=test_booking&type_menu=golf'" />
+	<img src="<?=$g4['path']?>/img/golf_banner06.png" width=675 height=300 onclick="location.href='<?=$g4['path']?>/bbs/board.php?bo_table=test_booking&type_menu=golf'" />
+	<img src="<?=$g4['path']?>/img/golf_banner07.png" width=675 height=300 onclick="location.href='<?=$g4['path']?>/bbs/board.php?bo_table=test_booking&type_menu=golf'" />
+</div>
+<div class="h05"></div>
+<div id="s3" >
+	<img src="<?=$g4['path']?>/img/condo_banner01.png" width=675 height=300 onclick="location.href='<?=$g4['path']?>/bbs/board.php?bo_table=test_booking&type_menu=condo'" />
+	<img src="<?=$g4['path']?>/img/condo_banner02.png" width=675 height=300 onclick="location.href='<?=$g4['path']?>/bbs/board.php?bo_table=test_booking&type_menu=condo'" />
+	<img src="<?=$g4['path']?>/img/condo_banner03.png" width=675 height=300 onclick="location.href='<?=$g4['path']?>/bbs/board.php?bo_table=test_booking&type_menu=hotel'" />
+	<img src="<?=$g4['path']?>/img/condo_banner04.png" width=675 height=300 onclick="location.href='<?=$g4['path']?>/bbs/board.php?bo_table=test_booking&type_menu=condo'" />
+	<img src="<?=$g4['path']?>/img/condo_banner05.png" width=675 height=300 onclick="location.href='<?=$g4['path']?>/bbs/board.php?bo_table=test_booking&type_menu=condo'" />
+	<img src="<?=$g4['path']?>/img/condo_banner06.png" width=675 height=300 onclick="location.href='<?=$g4['path']?>/bbs/board.php?bo_table=test_booking&type_menu=condo'" />
+	<img src="<?=$g4['path']?>/img/condo_banner07.png" width=675 height=300 onclick="location.href='<?=$g4['path']?>/bbs/board.php?bo_table=test_booking&type_menu=hotel'"/>
+	<img src="<?=$g4['path']?>/img/condo_banner08.png" width=675 height=300 onclick="location.href='<?=$g4['path']?>/bbs/board.php?bo_table=test_booking&type_menu=hotel'" />
+	<img src="<?=$g4['path']?>/img/condo_banner09.png" width=675 height=300 onclick="location.href='<?=$g4['path']?>/bbs/board.php?bo_table=test_booking&type_menu=hotel'" />
+</div>
+ 
+
+<?
+include_once("./_tail.php"); 
+?>
